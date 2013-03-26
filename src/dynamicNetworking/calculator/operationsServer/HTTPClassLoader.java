a6a2adb85dede5eb69e2700417067f40bc4e37e9
Steffen Olson <sknoslo@gmail.com>
2013-03-26 14:41:17 -0700

    Updated ClassLoader and OperationsServer

--- src/dynamicNetworking/calculator/operationsServer/HTTPClassLoader.java
+++ src/dynamicNetworking/calculator/operationsServer/HTTPClassLoader.java
@@ -4,17 +4,30 @@
  */
 package dynamicNetworking.calculator.operationsServer;
 
+import java.net.Socket;
+
 /**
  *
  * @author steffen
  */
 public class HTTPClassLoader extends ClassLoader {
-    public Class findClass(String className)
-    {
-        return null;
+    
+    Socket server;
+
+    public HTTPClassLoader(Socket server) {
+        this.server = server;
     }
-    public Class loadClassData(String className)
-    {
-        return null;
+
+    @Override
+    public Class findClass(String className) {
+        byte[] bytes = loadClassData(className);
+        return defineClass(className, bytes, 0, bytes.length);
+    }
+
+    public byte[] loadClassData(String className) {
+        byte[] bytes = null;
+        // TODO: get bytecode from server.
+        
+        return bytes;
     }
 }
--- src/dynamicNetworking/calculator/operationsServer/OperationsServer.java
+++ src/dynamicNetworking/calculator/operationsServer/OperationsServer.java
@@ -6,49 +6,70 @@ package dynamicNetworking.calculator.operationsServer;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author steffen
  */
 public class OperationsServer {
- 
-    public static void main( String[] args )
-    {
-    	Properties prop = new Properties();
- 
-    	try {
-               //load a properties file
-    		prop.load(new FileInputStream("OperationsServer.properties"));
- 
-               //get the property value and save
-                System.out.println(prop.getProperty("CLASS_ROOT_DIRECTORY"));
-    		System.out.println(prop.getProperty("HOST"));
-    		System.out.println(prop.getProperty("PORT"));
- 
-    	} catch (IOException ex) {
-    		ex.printStackTrace();
-        }
- 
+
+    private Properties prop;
+    private HTTPClassLoader classLoader;
+
+    public OperationsServer() {
+        this("OperationsServer.properties");
     }
-    
-    
-    
-    public Object getOperation(String operationString)
-    {
-        if(operationString.equals("*") ||
-           operationString.equals("/") ||
-           operationString.equals("+") ||
-           operationString.equals("-"))
-        {
-            return operationString;
+
+    public OperationsServer(String propFile) {
+        prop = new Properties();
+
+        try {
+            //load a properties file
+            prop.load(new FileInputStream(propFile));
+        } catch (IOException ex) {
+            System.err.println("Failed to open properties file.");
         }
-        else
+        
+        //get the property value and save
+        String rootDirectory = prop.getProperty("CLASS_ROOT_DIRECTORY");
+        System.out.println(rootDirectory);
+        String host = prop.getProperty("HOST");
+        System.out.println(host);
+        String port = prop.getProperty("PORT");
+        System.out.println(port);
+        
+        if (rootDirectory == null ||
+                host == null ||
+                port == null)
         {
+            System.err.println("Invalid properties file");
+        }
+        
+        int portNum = Integer.parseInt(port);
+        
+        try {
+            classLoader = new HTTPClassLoader(new Socket(host, portNum));
+        } catch (UnknownHostException ex) {
+            System.err.println("Unknown host: " + host);
+        } catch (IOException ex) {
+            System.err.println("IOException creating socket");
+        }
+    }
+
+    public Object getOperation(String operationString) {
+        if (operationString.equals("*")
+                || operationString.equals("/")
+                || operationString.equals("+")
+                || operationString.equals("-")) {
+            return operationString;
+        } else {
             System.out.println("not an operation");
             return null;
         }
     }
-   
 }
